5aa9ae2f54fa0ab6d15fad510c598bf7b051d509
MAX-XH <543322675@qq.com>
2025-05-24 14:21:07 +0800

    修改登录API

--- src/common.rs
+++ src/common.rs
@@ -996,7 +996,7 @@ fn get_api_server_(api: String, custom: String) -> String {
             return format!("http://{}", s);
         }
     }
-    "https://admin.rustdesk.com".to_owned()
+    "http://223.93.142.251:21114".to_owned()
 }
 
 #[inline]
